dd809f308e1c2c8b927677d1130507ea189170e4
etienne <etienne.reat@free.fr>
2020-12-24 15:22:40 +0100

    test gif animation ok

--- src/gif/FactoryGif.java
+++ src/gif/FactoryGif.java
@@ -1,4 +1,11 @@
 package gif;
 
+import javax.swing.*;
+import java.net.URL;
+
 public class FactoryGif{
+    public static JLabel get_label_gif(String path){
+        Icon icon = new ImageIcon(path);
+        return new JLabel(icon);
+    }
 }
--- src/test/test_gif.java
+++ src/test/test_gif.java
@@ -1,12 +1,26 @@
 package test;
 
+import gif.FactoryGif;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class test_gif extends JFrame{
     test_gif(String title){
         super(title);
+        setPreferredSize(new Dimension(1080,720));
+        JPanel panel_test = new JPanel();
+        panel_test.setBackground(Color.RED);
+        add(panel_test);
+        //setUndecorated(true);
+        panel_test.add(FactoryGif.get_label_gif("data/gif/witch/fly_witch.gif"));
+        panel_test.add(FactoryGif.get_label_gif("data/gif/witch/punch_witch.gif"));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
     public static void main(String[] argv) {
-        SwingUtilities.invokeLater(() -> new test_gif("Dowloader").setVisible(true));
+        SwingUtilities.invokeLater(() -> new test_gif("test gif").setVisible(true));
     }
 }
