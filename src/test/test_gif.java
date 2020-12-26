853e8c450e11090cdcdf0973dfb85d3879e0571f
etienne <etienne.reat@free.fr>
2020-12-26 02:39:40 +0100

    test

--- src/test/test_gif.java
+++ src/test/test_gif.java
@@ -8,17 +8,29 @@ import java.awt.*;
 public class test_gif extends JFrame{
     test_gif(String title){
         super(title);
-        setPreferredSize(new Dimension(1080,720));
         JPanel panel_test = new JPanel();
         panel_test.setBackground(Color.RED);
         add(panel_test);
-        //setUndecorated(true);
-        panel_test.add(FactoryGif.get_label_gif("data/gif/witch/fly_witch.gif"));
-        panel_test.add(FactoryGif.get_label_gif("data/gif/witch/punch_witch.gif"));
+        JLabel gif = FactoryGif.get_label_gif("data/gif/witch/punch_witch.gif");
+        panel_test.add(gif);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
+        new Thread(){
+            @Override
+            public void run(){
+                try {
+                    Thread.sleep(16 * 50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                JLabel gif = FactoryGif.get_label_gif("data/gif/witch/fly_witch.gif");
+                panel_test.removeAll();
+                panel_test.add(gif);
+                setVisible(true);
+            }
+        }.start();
     }
     public static void main(String[] argv) {
         SwingUtilities.invokeLater(() -> new test_gif("test gif").setVisible(true));
